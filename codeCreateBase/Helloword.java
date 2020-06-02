101f39da08d99ad74933b5e6ced2c1ba65c6b2e2
alai <2891864810@qq.com>
2020-06-02 23:24:22 +0800

    commit my helloword

---
+++ codeCreateBase/Helloword.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+			System.out.println("111");
+        System.out.println("Hello World");
+	    System.out.println("2222");
+		System.out.println("Hello World");
+		
+    }
+}
